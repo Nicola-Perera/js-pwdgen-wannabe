78a78aa79bf70914fd2cef4f8475d8e647bf7bef
Nicola-Perera <nicola.perera96@mail.com>
2021-10-08 17:52:18 +0200

    created new password and sent html page

--- main.js
+++ main.js
@@ -2,11 +2,16 @@
 in base ai dati inseriti dall'utente*/
 alert('Banvenuto/a! inserisci i dati richiesti e premi OK per creare una nuova password')
 
+//user name request
 let userFirstName = prompt('Scrivi il tuo nome');
 console.log(userFirstName);
 
+//user surname request
 let userSurname = prompt('Scrivi il tuo cognome');
 console.log(userSurname);
 
+//user favorite color request
 let userFavColor = prompt('Scrivi il tuo colore preferito');
 console.log(userFavColor);
+
+document.getElementById(pass).innerHTML = userFirstName + userSurname + userFavColor
