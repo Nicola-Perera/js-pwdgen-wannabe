5706f89c14f3fa0b0f87b2d54b58770749911e44
Nicola-Perera <nicola.perera96@mail.com>
2021-10-08 17:42:59 +0200

    add prompt reqest about main infos

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert('hello world')
+/* programma per creare password
+in base ai dati inseriti dall'utente*/
+alert('Banvenuto/a! inserisci i dati richiesti e premi OK per creare una nuova password')
+
+let userFirstName = prompt('Scrivi il tuo nome');
+console.log(userFirstName);
+
+let userSurname = prompt('Scrivi il tuo cognome');
+console.log(userSurname);
+
+let userFavColor = prompt('Scrivi il tuo colore preferito');
+console.log(userFavColor);
